8a300727c53965b52f6d7beb83a180b60ec70e6e
shriram.ethiraj <prasana.shri@gmail.com>
2021-08-15 01:42:46 +0530

    Over ride find all method in user repository to return users as list

--- src/main/java/com/udacity/jdnd/course3/critter/user/repository/UserRepository.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.udacity.jdnd.course3.critter.user.data.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findAll();
 }
