26aa2a945de20ae6d52cdf78c63ec33bbcae2838
shriram.ethiraj <prasana.shri@gmail.com>
2021-08-15 02:34:30 +0530

    Completed save pet endpoint

--- src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
@@ -1,5 +1,8 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.udacity.jdnd.course3.critter.user.data.User;
+import com.udacity.jdnd.course3.critter.user.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,10 +13,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/pet")
 public class PetController {
+    private final PetService petService;
+    private final UserService userService;
+
+    public PetController(PetService petService, UserService userService) {
+        this.petService = petService;
+        this.userService = userService;
+    }
 
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+       Pet pet = new Pet();
+       BeanUtils.copyProperties(petDTO, pet);
+       User user = userService.getUser(petDTO.getOwnerId());
+       pet.setUser(user);
+       Pet createdPet = petService.createPet(pet);
+       BeanUtils.copyProperties(createdPet, petDTO);
+       return petDTO;
     }
 
     @GetMapping("/{petId}")
--- src/main/java/com/udacity/jdnd/course3/critter/user/service/UserService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/service/UserService.java
@@ -1,10 +1,12 @@
 package com.udacity.jdnd.course3.critter.user.service;
 
+import com.udacity.jdnd.course3.critter.user.Exception.UserNotFoundException;
 import com.udacity.jdnd.course3.critter.user.data.User;
 import com.udacity.jdnd.course3.critter.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,5 +24,11 @@ public class UserService {
     public List<User> getAllUsers() {
         List<User> users = userRepository.findAll();
         return users;
+    }
+
+    public User getUser(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        User user = optionalUser.orElseThrow(UserNotFoundException::new);
+        return user;
     }
 }
