3edeea70cb9697065266b9cf9b464f27f4496aa4
shriram.ethiraj <prasana.shri@gmail.com>
2021-08-15 08:33:41 +0530

    Completed employees available endpoint

--- src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
+++ src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
@@ -1,9 +1,21 @@
 package com.udacity.jdnd.course3.critter.repository;
 
 import com.udacity.jdnd.course3.critter.data.Employee;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Long>  {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "select * from employee where id in" +
+            "(select a.employee_id from (select employee_id from employee_days_available where days_available = :dayOfWeek)" +
+            " a join\n" +
+            "(select employee_id, count(*) c from employee_skills where skills in :skillList group by employee_id having c = :skillListLength)" +
+            " b where a.employee_id = b.employee_id)", nativeQuery = true)
+    List<Employee> findAllAvailable(@Param("dayOfWeek") Integer dayOfWeek,@Param("skillList") ArrayList<Integer> skillList, @Param("skillListLength") Integer skillListLength);
 }
--- src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
@@ -3,11 +3,13 @@ package com.udacity.jdnd.course3.critter.service;
 import com.udacity.jdnd.course3.critter.Exception.EmployeeNotFoundException;
 import com.udacity.jdnd.course3.critter.data.Employee;
 import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
-import java.util.Optional;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
@@ -33,5 +35,12 @@ public class EmployeeService {
         employee.setDaysAvailable(daysAvailable);
         Employee updatedEmployee = employeeRepository.save(employee);
         return updatedEmployee;
+    }
+
+    public List<Employee> getEmployeesAvailable(LocalDate date, Set<EmployeeSkill> skillSet) {
+        Integer dayOfWeek = date.getDayOfWeek().ordinal();
+        ArrayList<Integer> skills = (ArrayList<Integer>) skillSet.stream().map(skill -> skill.ordinal()).collect(Collectors.toList());
+        List<Employee> employees = employeeRepository.findAllAvailable(dayOfWeek, skills, skills.size());
+        return  employees;
     }
 }
