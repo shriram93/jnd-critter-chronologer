41cba58dd95feee15cda011d60d829ce8c74587c
shriram.ethiraj <prasana.shri@gmail.com>
2021-08-15 05:34:22 +0530

    Added schedule entity and repository

---
+++ src/main/java/com/udacity/jdnd/course3/critter/data/Schedule.java
@@ -0,0 +1,70 @@
+package com.udacity.jdnd.course3.critter.data;
+
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    @JoinColumn(name="employee_id")
+    private List<Employee> employees;
+
+    @ManyToMany
+    @JoinColumn(name="pet_id")
+    private List<Pet> pets;
+
+    @Nationalized
+    private LocalDate date;
+
+    @ElementCollection(targetClass=EmployeeSkill.class)
+    private Set<EmployeeSkill> activities;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Set<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(Set<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
+}
